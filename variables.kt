b7eb4e2ce0a44ba564642a85dc06d1f69b7e9731
KhumaloAugustine <mr.a.s.khumalo@gmail.com>
2023-11-03 12:24:04 +0200

    Created a file for variables, where I learn about variables

---
+++ variables.kt
@@ -0,0 +1,8 @@
+/**
+ * A variable can be considered as container or storage box, where you can save date to be used later on in your program.
+ * When you create a variable you, telling a computer to reserve a specific amount of memory.
+ * once a variable is created, you can assign a value to it, and later you can retrieve/change its value.
+ * this happens throughout the lifespan of your program.
+ * variables have names and should be descriptive enough to give an idea of the data it holds.
+ * 
+ */
