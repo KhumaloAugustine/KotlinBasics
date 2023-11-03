775ed8e506f1e51406ae0987be28a979c9668286
KhumaloAugustine <mr.a.s.khumalo@gmail.com>
2023-11-03 12:44:49 +0200

    added a function where I assign num1 to 2 and print it value on the screen

--- HelloWorld.kt
+++ HelloWorld.kt
@@ -1,6 +1,10 @@
 /**
- * This is a simple function do display Hello, World message on the screen
+ * main function is considered as an entry point of our project
  */
+
+/*
+* This is a simple function do display Hello, World message on the screen
+* */
 fun main(){
-    println("Hello, World")
+    println("Hello, World!")
 }
--- variables.kt
+++ variables.kt
@@ -4,5 +4,13 @@
  * once a variable is created, you can assign a value to it, and later you can retrieve/change its value.
  * this happens throughout the lifespan of your program.
  * variables have names and should be descriptive enough to give an idea of the data it holds.
- * 
+ *
+ *
+ * The difference between val and var, is that we use val constant variables(immutable reference), and var can be changed
  */
+
+fun main(){
+    val num1 = 2
+
+    println("The value of num1 is $num1")
+}
