2b38ddfb1f4e7b023522d4516367438caf330d89
KhumaloAugustine <mr.a.s.khumalo@gmail.com>
2023-11-03 14:11:41 +0200

    added ReadingDataFromUser file which takes inputs from the user and display welcome message

---
+++ ReadingDataFromUser.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println ("What is your name ?")
+    val firstName = readln()
+
+    println("What is your surname ?")
+    val lastName = readln()
+
+    println("Welcome $firstName $lastName to my first Project!!")
+}
